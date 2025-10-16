f7a5dcd3404de85d84cfa605da2e075eb5b7d574
GrincenkoDaria <101104282+GrincenkoDaria@users.noreply.github.com>
2025-10-16 09:28:38 +0200

    Add files via upload

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,16 +1,4 @@
 <?php
 
-
-$profil = array(
-    0=> array(
-        'Jakub', 0.5, 'Banska Stiavnica'
-    ),
-    1 => array( 
-        'Martin', 203, 'Lodyn'
-    ),
-    2=> array(
-        'Matus', 18, "Zvolen"
-    )
-);
-
+define("TEXT", "Tato premenna je z config.php");    
 ?>
