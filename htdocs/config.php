b4980c1ea6fcfcb22303ae009726edebb89fe40e
GrincenkoDaria <101104282+GrincenkoDaria@users.noreply.github.com>
2025-10-16 09:27:32 +0200

    7.10.2025

---
+++ htdocs/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$profil = array(
+    0=> array(
+        'Jakub', 0.5, 'Banska Stiavnica'
+    ),
+    1 => array( 
+        'Martin', 203, 'Lodyn'
+    ),
+    2=> array(
+        'Matus', 18, "Zvolen"
+    )
+);
+
+?>
---
+++ htdocs/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moj WEB</title>
+    <link rel="stylesheet" href="style1.css">
+    <?php include("config.php"); ?>
+</head>
+<body>
+    <header>
+        <div class="header-log">
+            <img src="./assts/imgs/logoCat.png">
+        </div>
+        <nav>
+            <ul>
+                <li><a href = "./index.php">Uvod</a></li>
+                <li><a href = "./about.php">O mne</a></li>
+                <li><a href = "./contacts.php">Kontakty</a></li>
+                
+        </nav>
+    </header>
+    <main>
+        <h1>nadpis</h1>
+        <p>
+            <?php echo($profil[0][0]);?>
+        </p>
+    </main>
+
+    <aside></aside>
+   
+    <footer></footer>
+
+
+    
+</body>
+</html>
